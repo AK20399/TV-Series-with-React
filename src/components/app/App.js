5b6b6656daf9eed67b7b3ba309d2f0ea3c785d38
AK20399 <aakash.darji20@gmail.com>
2019-11-08 10:52:33 +0530

    Added State

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
 				<header className='App-header'>
 					<h1 className='App-title'>Tv Series</h1>
 				</header>
-				<Intro />
+				<Intro message='Here you can find all your loved Series' />
 			</div>
 		);
 	}
--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 class intro extends React.Component {
+	state = {
+		series: [],
+	};
+
 	render() {
-		return <p className='App-intro'>First Functional Component</p>;
+		return (
+			<div>
+				<p>{this.props.message}</p>
+				<p>The length of series array: {this.state.series.length}</p>
+			</div>
+		);
 	}
 }
 
