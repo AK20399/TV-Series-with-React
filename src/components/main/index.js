8d74f9cfce1309a9af13192ddbce784b17dc98d9
AK20399 <aakash.darji20@gmail.com>
2019-11-08 13:18:50 +0530

    Added SingleSeries

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Series from '../../container/series/index';
+import SingleSeries from '../../container/singleSeries';
 
 function main(props) {
 	return (
 		<Switch>
 			<Route exact path='/' component={Series} />
+			<Route exact path='/series:id' component={SingleSeries} />
 		</Switch>
 	);
 }
---
+++ src/container/singleSeries/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class singleSeries extends Component {
+	render() {
+		return (
+			<div>
+				<p>Single Series</p>
+			</div>
+		);
+	}
+}
+export default singleSeries;
