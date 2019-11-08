de1cefd2ecf637a45c9bacbd55a9f2eac40da8fd
AK20399 <aakash.darji20@gmail.com>
2019-11-08 12:03:51 +0530

    Conditional Rendering

--- src/container/series/index.js
+++ src/container/series/index.js
@@ -4,26 +4,40 @@ import SeriesList from '../../components/seriesList/index';
 class series extends Component {
 	state = {
 		series: [],
+		seriesName: '',
+		isFetching: false,
 	};
 
 	onSeriesInputChange = e => {
+		this.setState({ seriesName: e.target.value, isFetching: true });
+
 		fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
 			.then(response => response.json())
-			.then(json => this.setState({ series: json }));
+			.then(json => this.setState({ series: json, isFetching: false }));
 	};
 
 	render() {
+		const { series, seriesName, isFetching } = this.state;
+
 		return (
 			<div>
 				{/* <p>The length of series array: {this.state.series.length}</p> */}
 				<div>
 					<input
+						value={seriesName}
 						type='text'
 						onChange={this.onSeriesInputChange}
 						placeholder='Enter Series Name'
 					/>
 				</div>
-				<SeriesList list={this.state.series} />
+				{series.length === 0 && seriesName.trim() === '' && (
+					<p>Please Enter Series Name</p>
+				)}
+				{series.length === 0 && seriesName.trim() !== '' && (
+					<p>No Series has been Found By This Name</p>
+				)}
+				{isFetching === true && <p>.....Loading.....</p>}
+				{isFetching === false && <SeriesList list={this.state.series} />}
 			</div>
 		);
 	}
