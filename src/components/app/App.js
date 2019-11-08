c30810dc70d0af50fb292e8ee6e60e26d51c4f3f
AK20399 <aakash.darji20@gmail.com>
2019-11-08 10:57:23 +0530

    Added ComponentDidMount

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,18 @@ import Intro from '../intro/index';
 import './App.css';
 
 class App extends React.Component {
+	state = {
+		series: [],
+	};
+
+	componentDidMount() {
+		const series = ['vikings', 'game of thrones'];
+
+		setTimeout(() => {
+			this.setState({ series });
+		}, 1000);
+	}
+
 	render() {
 		return (
 			<div className='App'>
@@ -10,6 +22,7 @@ class App extends React.Component {
 					<h1 className='App-title'>Tv Series</h1>
 				</header>
 				<Intro message='Here you can find all your loved Series' />
+				<p>The length of series array: {this.state.series.length}</p>
 			</div>
 		);
 	}
--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 
 class intro extends React.Component {
-	state = {
-		series: [],
-	};
-
 	render() {
 		return (
 			<div>
 				<p>{this.props.message}</p>
-				<p>The length of series array: {this.state.series.length}</p>
 			</div>
 		);
 	}
